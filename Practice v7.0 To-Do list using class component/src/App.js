bfe97645088282c2dc026a0d6c45d7762238312c
DeepBhatt-Cyber <deepbhatt88666@gmail.com>
2024-03-02 17:03:53 +0530

    Create example for todo list using class component

---
+++ Practice v7.0 To-Do list using class component/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Main from "./Component/main";
+
+const App = () => {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+};
+
+export default App;
